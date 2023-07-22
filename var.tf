0e3ea626f78f67b7794481d1b88828bdce27ba81
nahsc0 <naseeryusuf07@gmail.com>
2023-07-22 14:55:43 +0100

    added public_ip variable

--- var.tf
+++ var.tf
@@ -3,3 +3,7 @@ description = "This server as the port number of the HTTP"
 type = number
 
 }
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the web server"
+}
