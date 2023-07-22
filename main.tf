c5a231fb54cfd8ec01428c16cca6873b50f30db2
nahsc0 <naseeryusuf07@gmail.com>
2023-07-22 17:18:49 +0100

    added some updates

--- main.tf
+++ main.tf
@@ -1,39 +1,64 @@
-# resource "aws_instance" "example" {
-#   ami                    = "ami-053b0d53c279acc90"
-#   instance_type          = "t2.micro"
-#   user_data              = <<-EOF
-#     #!/bin/bash
-#     echo "Hello, World" > index.html
-#     nohup busybox httpd -f -p 8000 &
-#   EOF
-#   user_data_replace_on_change = true
-#   tags = {
-#     Name = "terraform-example"
-#   }
-# }
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
 
+resource "aws_instance" "instance" {
+  ami                         = "ami-053b0d53c279acc90"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.instance.id]
+  key_name                    = "nasco"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    sudo apt install nginx
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+  EOF
+  user_data_replace_on_change = true
+  tags = {
+    Name = "terraform-instance"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # Replace with your desired region
+}
+
+data "http" "my_ip" {
+  url = "https://checkip.amazonaws.com/"
+}
+
+resource "aws_security_group" "instance" {
+  name_prefix = "instance-security-group"
+
+  # Allow inbound SSH (TCP port 22) from your public IP address
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-resource "aws_instance" "example" {
-  ami                          = "ami-053b0d53c279acc90"
-  instance_type                = "t2.micro"
-  vpc_security_group_ids       = [aws_security_group.instance.id]
-  user_data                    = <<-EOF
-    #!/bin/bash
-    echo "Hello, World" > index.html
-    nohup busybox httpd -f -p 8000 &
-  EOF
-  user_data_replace_on_change  = true
-  tags = {
-    Name = "terraform-example"
+
+  # Allow inbound HTTP (TCP port 80) from anywhere
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow inbound HTTPS (TCP port 443) from anywhere
+  ingress {
+    description = "HTTPS access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-
--- var.tf
+++ var.tf
@@ -1,9 +1,9 @@
-variable "server_port" {
-description = "This server as the port number of the HTTP" 
-type = number
+# variable "server_port" {
+# description = "This server as the port number of the HTTP" 
+# type = number
 
-}
+# }
 output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
+  value       = aws_instance.instance.public_ip
+  description = "The public IP address of the web server"
 }
